0af2bd87d8db87584becc5965bd81babfe8b9f80
Harri Siva <harrishiv6@gmail.com>
2022-11-07 00:45:39 -0500

    Took notes

---
+++ test.py
@@ -0,0 +1,9 @@
+def backtrack():
+    return
+
+def backtracking_serach():
+    # Select-Unassigned-Variable will use the minimum remaning values (MRV) heuristic (ones with the smallest domain lengths)
+    # Order-Domain-Values (the order at which domain values are tried, irrelevant? just use domain as is)    
+    # Or we could use the least_constraining_value (one that leaves the most number of blanks open?)
+
+    return
