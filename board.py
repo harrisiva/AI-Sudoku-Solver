9fa63f7860599b9b5b16620fe21f214a26453a3b
Harri Siva <harrishiv6@gmail.com>
2022-11-07 00:16:55 -0500

    Changed file structure

--- board.py
+++ board.py
@@ -1,58 +1,8 @@
-from classes import Graph, Variable, CSP, ac3
+from constants import *
+from classes import Graph, Variable, CSP
+from utilities import sudokuGraphify
+from functions import ac3
 import numpy as np
-SUDOKUDOMAIN = [1,2,3,4,5,6,7,8,9] # used in sudokuGraphify and main
-
-
-def get_adjacent(board,i,j): # Return a list of adjacent indexs as tuples, use it for the box perhaps and to create edges
-    adjacent = []
-    if i!=len(board[0])-1: adjacent.append([f'board[{i+1}][{j}]',i+1,j]) # Down (if not last row)
-    if i!=0: adjacent.append([f'board[{i-1}][{j}]',i-1,j])  # Up (if not first row)
-    if j!=len(board)-1: adjacent.append([f'board[{i}][{j+1}]',i,j+1]) # Right (if not right most col)
-    if j!=0: adjacent.append([f'board[{i}][{j-1}]',i,j-1]) # Left (if not left most col)
-    return adjacent
-
-def get_alldiff_constraints(board,i,j): # Given the puzzle and a cell's index, this function returns the all diff as a binary constraint relative to that cell
-    given = (i,j)
-    constraints = []
-    # Get all the other indexs in the same row and column as a tuple
-    for x in range(0,len(board),1):
-        if x==i: # if row in itteration==i(row of the element), append that entire row
-            for y in range(0,len(board[x]),1):
-                if (x,y)!=given: constraints.append(f'board[{i}][{j}]!=board[{x}][{y}]')
-        if (x,j)!=given: constraints.append(f'board[{i}][{j}]!=board[{x}][{j}]') 
-    return constraints
-
-def get_box_constraints(i,j):
-    # Load the box constratins from constraints.txt
-    with open('constraints.txt','r') as file: lines=[line.replace('\n','') for line in file.readlines()]
-    return [line for line in lines if f'board[{i}][{j}]' in line] 
-
-def get_constraints(board,i,j):
-    constraints = []
-    all_diff_constraints = get_alldiff_constraints(board,i,j)
-    for line in all_diff_constraints: constraints.append(line)
-    box_constraints = get_box_constraints(i,j)
-    for line in box_constraints: constraints.append(line)
-    return constraints
-
-def sudokuGraphify(board:list)->Graph:
-    graph = Graph() # Initialize a graph object, the board will be "moved" to this
-    # Nested for loop to convert the board to a graph
-    for i in range(0,len(board),1):
-        for j in range(0,len(board),1):
-
-            # Get the constraints related to the current node (nested list based)
-            constraints = get_constraints(board,i,j) # generate all the constraints for the current (from) node (i,j)
-            if board[i][j]!=0: constraints.append(f'board[{i}][{j}]=={board[i][j]}') # If the node has a value, add it to the constraints list as board[i][j]==value
-            
-            # Create a from_node, to_node (based on the adjacent list retrvied from the function called in the line below), and add both to the edge list with the list of constraints
-            adjacent = get_adjacent(board,i,j) # Get a list of adjacent cells (index based)
-            for cell in adjacent:
-                from_node = Variable(f'board[{i}][{j}]',i,j,board[i][j],SUDOKUDOMAIN if board[i][j]==0 else [board[i][j]]) # Last field to make sure that the domain for assigned variables is limited to the value assigned to it alone
-                to_node = Variable(cell[0],cell[1],cell[2],eval(cell[0]),SUDOKUDOMAIN if board[cell[1]][cell[2]]==0 else board[cell[1]][cell[2]])
-                graph.add_edge(from_node,to_node,constraints)
-
-    return graph
 
 if __name__=='__main__':
     # 0's are blank cells (no-assignments)
--- classes.py
+++ classes.py
@@ -1,6 +1,3 @@
-import math
-import copy
-
 class Variable:
     def __init__(self,name,i,j,value,domain):
         self.name = name #  String to represent the name of the node (in the map example)
@@ -49,57 +46,3 @@ class CSP:
         for variable in self.variables:
             returnable += f'{variable.name}: {variable.domain}\n'
         return returnable
-
-
-def revise(edge, board:list): 
-    # Set up the variables
-    xi:Variable = edge[0]
-    xiOriginal,constraints,revised = copy.deepcopy(xi.value), edge[2], False
-
-    # Make consistent (i.e., validate constraints and trim xi's domain)
-    domain_copy = copy.deepcopy(xi.domain) # Removals will be done to this list (as the original will be iterated on)
-    for x in xi.domain:
-        board[xi.i][xi.j]=x # Temporarily set the cell to hold x (for constraint evaluation purposes)
-        for constraint in constraints: 
-            if eval(constraint)==False: # Since the constraints for this CSP are alldiff based, remove x if any of the constraints are not met
-                if x in domain_copy: domain_copy.remove(x)
-
-    # Update xi's domain if a removal function was called and update revised value based on the latter
-    if len(domain_copy)!=len(xi.domain):
-        xi.domain=copy.deepcopy(domain_copy)
-        revised = True
-    
-    board[xi.i][xi.j] = copy.deepcopy(xiOriginal) # Reset the cell's value
-    return revised
-
-def ac3(csp:CSP, board:list): # NOTE: AC-3 is fine, there are no issues with it
-    queue:list = [edge for edge in csp.graph.edges] # add all the arcs (edges) from the csp's graph to the "queue"
-    while len(queue)>0:
-        arc = queue.pop() # order is irrelevant since it is commutative
-        if revise(arc,board): # if there were any x in the from nodes domain that would never work with the to domain and was therefore trimmed
-            if len(arc[0].domain)==0: return False # if no value in x, can be used as a consistent value with arc[0] with relation to arc[1], return false to indiciate that the problem is not solvable
-            for edge in csp.graph.edges:  # Find all neighbots to arc[0] <- find edges that have edge[1]==arc[0] and add them back to the queue
-                if edge[1]==arc[0]: queue.append(edge)
-    return True
-
-
-if __name__=='__main__': #To test AC-3 with the sqrt constraint example from m5-csp slide 17
-    # Test the Graph DS and AC-3 with slide 17 example
-    CSP_domain = [0,1,2,3,4,5,6,7,8,9]
-    # Create the variables with a variable name and a common domain
-    x = Variable('X',-1,CSP_domain)
-    y = Variable('Y',-1,CSP_domain)
-    # Create the graph by inserting edges
-    graph = Graph() # Initialize the graph object
-    # Add the edges to the graph (with constraints)
-    graph.add_edge(x,y,['x==math.sqrt(y)'])
-    graph.add_edge(y,x,['(y*y)==x'])
-    # Initializing CSP with the graph
-    csp = CSP(graph)
-    # Add the variables to the CSP
-    csp.variables.append(x)
-    csp.variables.append(y)
-    csp.domain=CSP_domain # Set the common domain of values
-    # Set the constraints (in this case, just copy them from the edges to the csp data structure)
-    for edge in graph.edges: csp.constaints.append(edge[2])
-    if ac3(csp): print(csp)
