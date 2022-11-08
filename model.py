06a19bc4d3eff41e0cfab651fa22f9596a52c6e9
Harri Sivakumar <siva6770@mylaurier.ca>
2022-11-07 19:54:36 -0500

    Moved loadSudoku function to model.py and updated the function to only append variable:value if there is a value for the variable (cell) in the board

--- model.py
+++ model.py
@@ -1,10 +1,5 @@
+from constants import *
 # MODEL 2
-
-
-# simpler DS:
-domains = {} # variable domain dictionary
-assignments = {} # assignment dictionary
-constaints = {} # variable constraint dictionary
 
 board = [
     [4,8,3, 9,2,1, 6,5,7],
@@ -20,11 +15,42 @@ board = [
     [6,9,5, 4,1,7, 0,8,2],
 ]
 
-INITIAL_DOMAIN = [1,2,3,4,5,6,7,8,9]
+def loadSudoku(board):
+    variables = []
+    indexes = {}
+    domains = {}
+    assignments = {}
+    constraints = {}
 
+    with open('map_constraints.txt','r') as file: BOXCONSTRAINTS=[line.replace('\n','') for line in file.readlines()] # Load box constraints from map_constraints file
+    for i in range(0,len(board),1):
+        for j in range(0,len(board[i]),1):
+            name = f'{ROW_INDEX_AS_KEY[i]}{j+1}' # j+1 because we are mapping 0 index as 1
+            domain = INITIAL_STANDARD_DOMAIN if board[i][j]==0 else [board[i][j]]
+            value = board[i][j]
+            constraints_list = []
+            
+            # Generate the alldiff constraints for this variable
+            for row in range(len(board)):
+                for col in range(len(board[row])):
+                    if row == i and col!=j:
+                        constraints_list.append(f'{name}!={ROW_INDEX_AS_KEY[row]}{col+1}')
+                    if col == j and row != i:
+                        constraints_list.append(f'{name}!={ROW_INDEX_AS_KEY[row]}{col+1}')
 
-# to view, just map the assignment dictionary to a matrix (have map as a separate dictionary)
-# for values not in the assignemnt dictionary, just leave a zero in place 
+            # Retrive the box constraints for this variable (where this variable is the from node)
+            for constraint in BOXCONSTRAINTS: 
+                if name in constraint[:2]: constraints_list.append(constraint)
+
+            variables.append(name)
+            indexes[name]=[i,j]
+            domains[name]=domain
+            if value!=0: assignments[name]=value # NOTE: Append variable:value only if variable has a value assigned to it (meaning !=0 since 0's are blank)
+            constraints[name]=constraints_list
+
+    return variables, indexes, domains, assignments, constraints
+
+variables, indexes, domains, assignments, constraints = loadSudoku(board)
 
 # for ac-3
 # take a vairbale that is not in the assignment dictionary
